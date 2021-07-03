df0a1f1b079c4edb7f25a6e7e121a8336d291043
Breazzz <breazzz.mail@yandex.ru>
2021-07-03 21:35:29 +0300

    Action creators, refactor

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
+import {ADD_CASH, ADD_CUSTOMER, GET_CASH, REMOVE_CUSTOMER} from "./store/actions";
+import {addCustomerAction, removeCustomerAction} from "./store/customerReducer";
 
 function App() {
   const dispatch = useDispatch()
   const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
 
   const addCash = (cash) => {
-    dispatch({type: "ADD_CASH", payload: cash})
+    dispatch({type: ADD_CASH, payload: cash})
   }
 
   const getCash = (cash) => {
-    dispatch({type: "GET_CASH", payload: cash})
+    dispatch({type: GET_CASH, payload: cash})
+  }
+
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(customer))
+  }
+
+  const removeCustomer = (customer) => {
+    dispatch(removeCustomerAction(customer.id))
   }
 
   return (
@@ -26,6 +41,25 @@ function App() {
         <button onClick={() => getCash(Number(prompt()))}>
           Снять со счета
         </button>
+        <button onClick={() => addCustomer(prompt())}>
+          Добавить клиента
+        </button>
+      </div>
+      <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
+        {customers.length > 0 ? (
+            <ul style={{color: "darkgreen", fontWeight: "900"}}>
+              {customers.map(customer => (
+                <li>
+                  {customer.name} <span onClick={() => removeCustomer(customer)} style={{cursor: "pointer"}}>❌</span>
+                </li>
+              ))}
+            </ul>
+          )
+          : (
+            <div style={{color: 'tomato'}}>
+              Клиентов нет
+            </div>
+          )}
       </div>
     </div>
   );
--- src/store/cashReducer.js
+++ src/store/cashReducer.js
@@ -1,12 +1,14 @@
+import {ADD_CASH, GET_CASH} from "./actions";
+
 const defaultState = {
   cash: 0,
 }
 
 export const cashReducer = (state = defaultState, action) => {
   switch (action.type) {
-    case "ADD_CASH":
+    case ADD_CASH:
       return {...state, cash: state.cash + action.payload}
-    case "GET_CASH":
+    case GET_CASH:
       return {...state, cash: state.cash - action.payload}
     default:
       return state
